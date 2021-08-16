223d768f4d65398850e91d20072aa83b10d1e6d8
Ciucur Daniel <ciucur.daniel14@gmail.com>
2021-08-16 20:01:03 +0300

    Initial commit

---
+++ bin/sum_of_two_numbers/README.md
@@ -0,0 +1,11 @@
+# Sum of two numbers
+
+Create a function that returns the sum of two numbers.
+
+## Example
+
+```dart
+addition(4,5) => 9
+addition(4,4) => 8
+addition(4,50) => 54
+```
---
+++ bin/sum_of_two_numbers/sum_of_two_numbers.dart
@@ -0,0 +1,13 @@
+int addition(int a, int b){
+  return a + b;
+}
+
+
+void main(){
+
+  print('Addition function in action:');
+
+  print(addition(4, 5));
+  print(addition(4, 4));
+  print(addition(4, 50));
+}
